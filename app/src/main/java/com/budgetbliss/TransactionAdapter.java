06ce0dfa8c81becd7e8b83cdc398ff560ba2bdfc
Shaniya Sen <shaniyasen20@gmail.com>
2025-05-22 22:40:31 +1200

    Update TransactionAdapter.java

--- app/src/main/java/com/budgetbliss/TransactionAdapter.java
+++ app/src/main/java/com/budgetbliss/TransactionAdapter.java
@@ -1,34 +1,63 @@
 package com.example.budgetbliss;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.ArrayList;
+
 public class TransactionAdapter extends RecyclerView.Adapter<TransactionAdapter.MyViewHolder> {
+
+    Context context;
+    ArrayList<TransactionModel> transactionModelArrayList;
+
+    public TransactionAdapter(Context context, ArrayList<TransactionModel> transactionModelArrayList) {
+        this.context = context;
+        this.transactionModelArrayList = transactionModelArrayList;
+    }
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.one_recycler_item, parent, false);
+        return new MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        // TODO: Bind data to views here
+        TransactionModel model = transactionModelArrayList.get(position);
+
+        if (model.getType().equals("Expense")) {
+            holder.priority.setBackgroundResource(R.drawable.red_shape);
+        } else {
+            holder.priority.setBackgroundResource(R.drawable.green_shape);
+        }
+
+        holder.amount.setText(model.getAmount());
+        holder.date.setText(model.getDate());
+        holder.note.setText(model.getNote());
     }
 
     @Override
     public int getItemCount() {
-        return 0; // TODO: Return actual list size
+        return transactionModelArrayList.size();
     }
 
     public static class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView note, amount, date;
+        View priority;
+
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
-            // TODO: Initialize views here
+            note = itemView.findViewById(R.id.note_one);
+            amount = itemView.findViewById(R.id.amount_one);
+            date = itemView.findViewById(R.id.date_one);
+            priority = itemView.findViewById(R.id.priority_one);
         }
     }
 }
