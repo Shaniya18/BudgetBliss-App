07a0f062ac2df7e886d5a18b57b415bfcad7abd5
Shaniya Sen <shaniyasen20@gmail.com>
2025-05-22 23:25:12 +1200

    Update AddTransactionActivity.java

--- app/src/main/java/com/budgetbliss/AddTransactionActivity.java
+++ app/src/main/java/com/budgetbliss/AddTransactionActivity.java
@@ -6,7 +6,6 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-
 import com.example.budgetbliss.databinding.ActivityAddTransactionBinding;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -14,77 +13,91 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 import java.util.UUID;
 
 public class AddTransactionActivity extends AppCompatActivity {
+
     ActivityAddTransactionBinding binding;
-     FirebaseFirestore fStore;
-     FirebaseAuth firebaseAuth;
-     FirebaseUser firebaseUser;
-    String type ="";
+    FirebaseFirestore fStore;
+    FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+    String type = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding=ActivityAddTransactionBinding.inflate(getLayoutInflater());
+        binding = ActivityAddTransactionBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        fStore=FirebaseFirestore.getInstance();
-        firebaseAuth=firebaseAuth.getInstance();
-        firebaseUser=firebaseAuth.getCurrentUser();
 
+        fStore = FirebaseFirestore.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
+
+        // Checkbox listeners
         binding.expenseCheckBox.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                type="Expense";
+                type = "Expense";
                 binding.expenseCheckBox.setChecked(true);
                 binding.incomeCheckBox.setChecked(false);
             }
         });
+
         binding.incomeCheckBox.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                type="Income";
+                type = "Income";
                 binding.expenseCheckBox.setChecked(false);
                 binding.incomeCheckBox.setChecked(true);
             }
         });
 
+        // Add transaction button
+        binding.btnAddTransaction.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String amount = binding.userAmountAdd.getText().toString().trim();
+                String note = binding.userNoteAdd.getText().toString().trim();
 
-        binding.btnAddTransaction.setOnClickListener(new View.OnClickListener(){
-          @Override
-          public void onClick(View v){
-              String amount=binding.userAmountAdd.getText().toString().trim();
-              String note =binding.userNoteAdd.getText().toString().trim();
-              if(amount.length()<=0){
-                  return;
-              }
-              if(type.length()<=0){
-                  Toast.makeText(AddTransactionActivity.this, "Select transaction type", Toast.LENGTH_SHORT).show();
-              }
-              String id=UUID.randomUUID().toString();
-              Map<String,Object>transaction=new HashMap<>();
-              transaction.put("id",id);
-              transaction.put("amount",amount);
-              transaction.put("note",note);
-              transaction.put("type",type);
-              fStore.collection("Expenses").document(firebaseAuth.getUid()).collection("Notes").document(id).set(transaction)
-                      .addOnSuccessListener(new OnSuccessListener<Void>() {
-                          @Override
-                          public void onSuccess(Void unused) {
-                              Toast.makeText(AddTransactionActivity.this, "Added", Toast.LENGTH_SHORT).show();
-                              binding.userNoteAdd.setText(" ");
-                              binding.userAmountAdd.setText("");
-                          }
-                      })
-                      .addOnFailureListener(new OnFailureListener() {
-                          @Override
-                          public void onFailure(@NonNull Exception e) {
-                              Toast.makeText(AddTransactionActivity.this,e.getMessage(), Toast.LENGTH_SHORT).show();
-                          }
-                      });
+                if (amount.length()<=0) {
+                    return;
+                }
 
-          }
+                if(type.length()<=0){
+                    Toast.makeText(AddTransactionActivity.this, "Select transaction type", Toast.LENGTH_SHORT).show();
+                }
+                String id = UUID.randomUUID().toString();
+                // Transaction map
+                Map<String, Object> transaction = new HashMap<>();
+                transaction.put("id", id);
+                transaction.put("amount",amount);
+                transaction.put("note", note);
+                transaction.put("type", type);
+
+
+                fStore.collection("Data History").document(firebaseAuth.getUid()).collection("Notes").document(id).set(transaction)
+
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Toast.makeText(AddTransactionActivity.this, "Added", Toast.LENGTH_SHORT).show();
+
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(AddTransactionActivity.this,e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
         });
     }
 }
