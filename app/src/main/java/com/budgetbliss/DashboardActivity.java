64e8ac3d419b33c3676f9777a12330d8e7728575
Shaniya Sen <shaniyasen20@gmail.com>
2025-05-22 22:31:23 +1200

    Update DashboardActivity.java

--- app/src/main/java/com/budgetbliss/DashboardActivity.java
+++ app/src/main/java/com/budgetbliss/DashboardActivity.java
@@ -2,20 +2,51 @@ package com.example.budgetbliss;
 
 import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
 import android.view.View;
 
 
 
 import com.example.budgetbliss.databinding.ActivityDashboardBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class DashboardActivity extends AppCompatActivity {
     ActivityDashboardBinding binding;
+    FirebaseFirestore firebaseFirestore;
+    FirebaseAuth firebaseAuth;
+
+
+    ArrayList<TransactionModel> transactionModelArrayList;
+    TransactionAdapter transactionAdapter;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityDashboardBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        firebaseAuth = FirebaseAuth.getInstance();
+        transactionModelArrayList = new ArrayList<>();
+
+
+        binding.historyRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        binding.historyRecyclerView.setHasFixedSize(true);
+
+
+
+
         binding.addFloatingBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -27,6 +58,31 @@ public class DashboardActivity extends AppCompatActivity {
             }
         });
 
-            }
-        }
+        loadData();
 
+
+    }
+
+    private void loadData() {
+        firebaseFirestore.collection("DataHistory").document(firebaseAuth.getUid()).collection("Notes").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                for (DocumentSnapshot ds : task.getResult()) {
+                    TransactionModel model = new TransactionModel(
+                            ds.getString("id"),
+                            ds.getString("note"),
+                            ds.getString("amount"),
+                            ds.getString("type"),
+                            ds.getString("date"));
+                    transactionModelArrayList.add(model);
+                    transactionAdapter=new TransactionAdapter(DashboardActivity.this,transactionModelArrayList);
+                    binding.historyRecyclerView.setAdapter(transactionAdapter);
+
+
+                }
+
+            }
+        });
+
+    }
+}
